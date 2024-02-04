4b6f9ceee37ac0695767e0acbb7d52e85bf4e6c4
Fílip Anselmo <filipanselmo870@gmail.com>
2024-02-04 10:19:31 -0400

    feat: add método de validação de arquivos

--- src/controllers/ApiGameImageController.ts
+++ src/controllers/ApiGameImageController.ts
@@ -2,6 +2,7 @@ import { Request, Response } from "express";
 import { CreateGameImageService } from "../services/CreateGameImageService";
 import { GameImageRepository } from "../repositories/GameImageRepository";
 import { ListGameImagesService } from "../services/ListGameImagesService";
+import mime from "mime";
 // import multer from "multer";
 
 // const storage = multer.diskStorage({
@@ -16,10 +17,24 @@ import { ListGameImagesService } from "../services/ListGameImagesService";
 
 // const upload = multer({ storage: storage });
 
+// const validarTipoArquivo = (fileName) => {
+//     const tipoMime = mime.getType(fileName);
+//     const tipoPermitido = ['image/jpeg', 'image/png'];
+//     if (tipoPermitido.includes(tipoMime)) {
+//         console.log(`Tipo de arquivo válido: ${tipoMime}`);
+//         return true;
+//     } else {
+//         console.log(`Tipo de arquivo inválido: ${tipoMime}`);
+//         return false;
+//     }
+// } 
+
 export default {
     createGameImage: async (request: Request, response: Response) => {
         try {
             const { filename } = request.file;
+
+            // const fileNameValidado = validarTipoArquivo(filename);
 
             const createGameImage = new CreateGameImageService(new GameImageRepository());
 
