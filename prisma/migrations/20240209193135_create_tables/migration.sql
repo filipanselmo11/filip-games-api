64f735247b52c8aa8c55a94b6c75b1d550d9c7e0
Fílip Anselmo <filipanselmo870@gmail.com>
2024-02-09 15:39:47 -0400

    feat: atualizando algumas coisas

---
+++ prisma/migrations/20240209193135_create_tables/migration.sql
@@ -0,0 +1,20 @@
+-- CreateTable
+CREATE TABLE `games` (
+    `id` INTEGER NOT NULL AUTO_INCREMENT,
+    `name` VARCHAR(191) NOT NULL,
+    `gender` VARCHAR(191) NOT NULL,
+    `available` BOOLEAN NOT NULL,
+    `price` VARCHAR(191) NOT NULL,
+    `createdAt` DATETIME(3) NULL DEFAULT CURRENT_TIMESTAMP(3),
+
+    PRIMARY KEY (`id`)
+) DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
+
+-- CreateTable
+CREATE TABLE `images` (
+    `id` INTEGER NOT NULL AUTO_INCREMENT,
+    `filename` VARCHAR(191) NOT NULL,
+    `createdAt` DATETIME(3) NULL DEFAULT CURRENT_TIMESTAMP(3),
+
+    PRIMARY KEY (`id`)
+) DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
--- src/server.ts
+++ src/server.ts
@@ -1,9 +1,11 @@
 import  Express  from "express";
 import { routes } from "./routes";
+import cors from 'cors';
 
 const app = Express();
 
 app.use(Express.json());
+app.use(cors())
 app.use(routes);
 
 const PORT = 3333;
